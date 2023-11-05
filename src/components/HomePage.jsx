426df6960516dc43377a7f46146f1d5c05fa388f
Gurpreet Singh <Singhamarjot9@yahoo.com>
2023-11-05 18:11:08 +0000

    Added Api ( changes color of heading )

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const HomePage = () => {
-return(
-    <React.Fragment>
-        <div class="">
-        <h1> hi </h1>
-        </div>
-        
-    </React.Fragment>
-)}
+
+function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://random-data-api.com/api/color/random_color')
+      .then(response => {
+        setPosts(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  })
+
+  const styles = {
+    coloring: {
+      color: posts.hex_value
+    }
+  }
+
+    return(
+        <div class="container">
+          <h1 style = {{color: styles.coloring.color}}> WELCOME TO THE CHRISTMAS WEBSITE!!! </h1>
+        </div>  
+      
+    )
+}
 
 export default HomePage
