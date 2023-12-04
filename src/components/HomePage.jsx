4ef28aa52a0f3c32e24791cb3603e357b3227650
Gurpreet Singh <Singhamarjot9@yahoo.com>
2023-12-04 01:54:54 +0000

    added greeting api in homepage.jsx (2nd api)

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Countdown from 'react-countdown';
+import ReactDOM from 'react-dom';
 
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
+  const [greets, setGreetings] = useState("");
 
   useEffect(() => {
-    axios.get('https://random-data-api.com/api/color/random_color')
+    axios.get('https://random-data-api.com/api/color/random_color') 
       .then(response => {
         setPosts(response.data);
         console.log(response.data);
       })
       .catch(error => {
-        console.error(error);
+        console.error(error); 
       });
   })
+  
+  useEffect(() => {
+    axios.get('https://christmasjoy.dev/api/greetings?random')
+  .then((res) => {
+    setGreetings(res.data);
+    //console.log(res.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+  }, []);
+  
 
   const styles = {
     coloring: {
@@ -23,22 +40,24 @@ function HomePage() {
   }
 
     return(
+      <React.Fragment>
         <div class="container">
           <br></br>
           <h1 style = {{color: styles.coloring.color}}> WELCOME TO THE CHRISTMAS WEBSITE!!! </h1>
           <br></br>
-          <p> Welcome to our website! We hope you have a merry Christmas and a happy holiday season!   </p>
+          <p class="christmasFont"> Welcome to our website! We hope you have a merry Christmas and a happy holiday season!   </p>
           <br></br>
+          
           <p>Christmas is an annual holiday celebrated by millions worldwide. Christmas is a time of </p>
           <p>togetherness and giving and is often associated with festive decorations and the exchange of </p>
           <p>gifts. Over time, Christmas has evolved from lighting homes with colorful decorations to singing </p>
           <p>carols. It's a season of love and sharing, making it one of the most cherished holidays globally. </p>
+          <h3> {greets} </h3>
           <iframe src="https://christmascountdown.live/fullscreen" height="500" width="500" title="song1"></iframe>
         </div> 
+      </React.Fragment>
         
         
-         
-      
     )
 }
 
