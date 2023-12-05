214dc7fba3636c30ab75b917e3a5da70c3c7b86e
Gurpreet Singh <Singhamarjot9@yahoo.com>
2023-12-05 20:42:34 +0000

    updated api links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
         <div class="footerwrap">
         <p> Gurpreet Singh & Michaela H</p>
         <p>
-        <a href="https://christmasjoy.dev/docs/getting-started">Countdown & Greeting API</a>
+        <a href="https://christmasjoy.dev/docs/getting-started">Greeting API</a>
         <a href="https://random-data-api.com/"> Color API</a> 
+        <a href="https://christmascountdown.live/api"> Countdown API </a>
         </p>
-        
         </div>
     )
 }
